169efaf724f31801439c3f13e487ef8e70361cbf
Masle16 <mathiias@live.dk>
2020-10-30 09:48:41 +0100

    added python controller and started on anc

---
+++ pi_control/samples/python/controller/main.py
@@ -0,0 +1,139 @@
+"""
+Controller for the robot.
+"""
+
+
+from time import sleep, time
+from math import pi, sin
+from matrix_lite import gpio
+from matrix_lite import led
+
+
+# GPIO  via Matrix Voice
+TB6612_RIGHT_MOTOR_PWMA = 14    # (Orange)
+TB6612_LEFT_MOTOR_PWMB = 8      # (Green)
+TB6612_RIGHT_MOTOR_AIN1 = 12    # (Blue)
+TB6612_RIGHT_MOTOR_AIN2 = 10    # (Brown)
+TB6612_LEFT_MOTOR_BIN1 = 6      # (Grey)
+TB6612_LEFT_MOTOR_BIN2 = 4      # (Pink)
+
+# Matrix Voice LEDs
+MATRIX_LED_R_1 = 0
+MATRIX_LED_R_2 = 1
+MATRIX_LED_R_3 = 2
+MATRIX_LED_R_4 = 3
+MATRIX_LED_R_5 = 4
+MATRIX_LED_R_6 = 5
+MATRIX_LED_R_7 = 6
+MATRIX_LED_R_8 = 7
+MATRIX_LED_R_9 = 8
+
+MATRIX_LED_L_1 = 9
+MATRIX_LED_L_2 = 10
+MATRIX_LED_L_3 = 11
+MATRIX_LED_L_4 = 12
+MATRIX_LED_L_5 = 13
+MATRIX_LED_L_6 = 14
+MATRIX_LED_L_7 = 15
+MATRIX_LED_L_8 = 16
+MATRIX_LED_L_9 = 17
+
+
+def init_gpio_pins()->None:
+    """Init GPIO pins.
+    """
+    # Right motor
+    gpio.setMode(TB6612_RIGHT_MOTOR_PWMA, 'output')
+    gpio.setFunction(TB6612_RIGHT_MOTOR_PWMA, 'PWM')
+    gpio.setMode(TB6612_RIGHT_MOTOR_AIN1, 'output')
+    gpio.setMode(TB6612_RIGHT_MOTOR_AIN2, 'output')
+
+    # Left motor
+    gpio.setMode(TB6612_LEFT_MOTOR_PWMB, 'output')
+    gpio.setFunction(TB6612_LEFT_MOTOR_PWMB, 'PWM')
+    gpio.setMode(TB6612_LEFT_MOTOR_BIN1, 'output')
+    gpio.setMode(TB6612_LEFT_MOTOR_BIN2, 'output')
+
+
+def set_left_speed_direction(speed:int, direction:int)->None:
+    # Reverse
+    if direction <= 0:
+        gpio.setDigital(TB6612_LEFT_MOTOR_BIN1, 'OFF')
+        gpio.setDigital(TB6612_LEFT_MOTOR_BIN2, 'ON')
+
+    # Forward
+    if direction > 0 or direction >= 1:
+        gpio.setDigital(TB6612_LEFT_MOTOR_BIN1, 'ON')
+        gpio.setDigital(TB6612_LEFT_MOTOR_BIN2, 'OFF')
+
+    # Set motor spped via PWM signal
+    if speed > 100: speed = 100
+    if speed < 0: speed = 0
+    gpio.setPWM({'pin': TB6612_LEFT_MOTOR_PWMB, 'precentage': speed, 'frequency': 1000})
+
+
+
+def set_right_speed_direction(speed:int, direction:int)->None:
+    # Reverse
+    if direction <= 0:
+        gpio.setDigital(TB6612_RIGHT_MOTOR_AIN1, 'OFF')
+        gpio.setDigital(TB6612_RIGHT_MOTOR_AIN2, 'ON')
+
+    # Forward
+    if direction > 0 or direction >= 1:
+        gpio.setDigital(TB6612_RIGHT_MOTOR_AIN1, 'ON')
+        gpio.setDigital(TB6612_RIGHT_MOTOR_AIN2, 'OFF')
+
+    # Set motor spped via PWM signal
+    if speed > 100: speed = 100
+    if speed < 0: speed = 0
+    gpio.setPWM({'pin': TB6612_RIGHT_MOTOR_PWMA, 'precentage': speed, 'frequency': 1000})
+
+
+def startup_show_led_rainbow()->None:
+    everloop = ['black'] * led.length
+
+    led_adjust = 0.0
+    if len(everloop) == 35: led_adjust = 0.51   # Matrix creator
+    else: led_adjust = 1.01                     # Matrix Voice
+
+    frequency = 0.375
+    counter = 0.0
+    tick = len(everloop) - 1
+
+    rainbow_start = time()
+    rainbow_elapsed = time() - start_time
+
+    while elapsed_time < 30.0:
+        # Create rainbow
+        for i in range(len(everloop)):
+            r = round(max(0, (sin(frequency*counter+(pi/180*240))*155+100)/10))
+            g = round(max(0, (sin(frequency*counter+(pi/180*120))*155+100)/10))
+            b = round(max(0, (sin(frequency*counter)*155+100)/10))
+
+            counter += led_adjust
+            everloop[i] = {'r': r, 'g': g, 'b': b}
+
+            # Slowly show rainbow
+            if tick != 0:
+                for i in reversed(range(tick)): everloop[i] = {}
+                tick -= 1
+            
+            led.set(everloop)
+            sleep(0.35)
+        
+        rainbow_elapsed = time() - rainbow_start
+
+    sleep(1.0)
+    led.set('black')
+
+
+def main():
+    print(f'LEDs on the MATRIX device --> {led.length}')
+    startup_show_led_rainbow()
+
+    init_gpio_pins()
+
+
+if __name__ == "__main__":
+    main()
